41d1a13a585f032a3ce2d5e840773ad5ebb4165e
root <root@DESKTOP-RHA5CJR.localdomain>
2024-09-01 03:15:38 +0300

    list comprehension

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
+
 def return_evens(num_list):
-    pass
+    evens = [n for n in num_list if n % 2 == 0]
+    print(evens if evens else [])
+    return evens
+   
 
 def make_exclamation(sentence_list):
-    pass
+    return [(sentence + '!') for sentence in sentence_list]
